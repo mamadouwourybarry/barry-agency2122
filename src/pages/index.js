92bf05e615a7ed11d5f210144178a503b5055e75
Mamadou Barry <mamadou.barry01@student.ap.be>
2021-12-30 19:46:33 +0100

    Gatsby plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
-      <Layout pageTitle="Welcome to Barry Agency!">
-      <p>Lorem ipsum</p>
+    <main>
+      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
+        <p>Lorem ipsum</p>
+        <StaticImage
+          alt="home baner!"
+          src="../images/home-banner-hd.jpg"
+        />
       </Layout>
+    </main>
   )
 }
 
